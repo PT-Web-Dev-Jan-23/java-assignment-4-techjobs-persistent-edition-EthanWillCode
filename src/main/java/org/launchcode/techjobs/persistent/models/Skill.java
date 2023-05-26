6499653853aef45a92786e914166c4a82229b173
Ethan Irvin <aethil@yahoo.com>
2023-05-25 19:54:17 -0500

    Building Employer and Skill Repositories. Adding index method to EmployerController.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,15 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 100, message = "Please enter a valid name.")
+    private String description;
+
+    public Skill() {}
 }
