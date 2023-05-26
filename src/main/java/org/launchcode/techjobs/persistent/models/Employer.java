274f4863b4632d47973505c5cce7feefcd2f438b
Ethan Irvin <aethil@yahoo.com>
2023-05-25 19:19:45 -0500

    Updating Validation for AbstractEntity and Employer.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(min = 3, max = 100, message = "Please enter a valid name.")
+    private String location;
 
-
+    public Employer() {}
 }
